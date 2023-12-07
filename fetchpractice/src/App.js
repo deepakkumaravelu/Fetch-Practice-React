952e3c9067189ad947a04b437f192ded3c05d045
deepak <deepak1122003kumar@gmail.com>
2023-12-07 22:40:41 +0530

    axios practice

--- fetchpractice/src/App.js
+++ fetchpractice/src/App.js
@@ -1,18 +1,21 @@
 import {useEffect, useState} from 'react';
+import Axios from "axios";
 import './App.css';
 function App(){
  const[m,setM]=useState("");
- fetch(`http://www.omdbapi.com/?s=leo&apikey=383bfd25`)
- .then((res)=>res.json())
- .then((data)=>console.log(data));
+ const[year,setYear]=useState("");
+
+ Axios.get(`http://www.omdbapi.com/?s=${m}&apikey=383bfd25`).then((res)=>{
+  console.log(res.data.Year);
+ })
   return(
     <div className='App' >
-    <form>
-    
+    <form onSubmit={(e)=>e.preventDefault()}>
     <label>input movie name</label>
-    <input onChange={(e)=>setM(e.target.value)}></input>
-    
+    <input type="text" onChange={(e)=>setM(e.target.value)}></input>
+    <input type="submit" value="Save"></input>
     </form>
+    
     </div>
   );
 }
