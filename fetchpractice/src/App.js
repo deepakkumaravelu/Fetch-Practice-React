f3d58549e6df46981f9cf7ca0eba9ea21f5c854d
deepakkumaravelu <deepak1122003kumar@gmail.com>
2023-12-07 16:05:59 +0530

    omdb api practice

--- fetchpractice/src/App.js
+++ fetchpractice/src/App.js
@@ -1,16 +1,18 @@
 import {useEffect, useState} from 'react';
 import './App.css';
 function App(){
-  const[imgurl,setImgurl]=useState('');
-  useEffect(()=>{
-    const fetchcatimage=async()=>{
-        const response=await fetch('https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=beng&api_key=YOUR_API_KEY');
-        const data=await response.json();
-        setImgurl(data[0].url);
-    }; fetchcatimage();},[]);
+ const[m,setM]=useState("");
+ fetch(`http://www.omdbapi.com/?s=leo&apikey=383bfd25`)
+ .then((res)=>res.json())
+ .then((data)=>console.log(data));
   return(
     <div className='App' >
-    {imgurl && <img src={imgurl} alt="Cat" className="imm" />}
+    <form>
+    
+    <label>input movie name</label>
+    <input onChange={(e)=>setM(e.target.value)}></input>
+    
+    </form>
     </div>
   );
 }
